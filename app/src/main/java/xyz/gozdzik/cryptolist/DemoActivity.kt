d08e9cfeeba7314ad728608806b2437e89e0118c
sgozdzik <40645632+sgozdzik@users.noreply.github.com>
2022-03-31 17:36:44 +0200

    [1] Add init screens with navigation controller (#4)
    * Add init screens with navigation controller
    
    * Code clean

--- app/src/main/java/xyz/gozdzik/cryptolist/DemoActivity.kt
+++ app/src/main/java/xyz/gozdzik/cryptolist/DemoActivity.kt
@@ -3,9 +3,9 @@ package xyz.gozdzik.cryptolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_demo)
     }
 }
