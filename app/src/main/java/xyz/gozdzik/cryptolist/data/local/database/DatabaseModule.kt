aef540e3b6bde9abe94fdbd71a5ba63015b0f75e
Szymon Gozdzik <szymon@gozdzik.xyz>
2022-04-12 17:51:48 +0200

    Clean prepopulating database

--- app/src/main/java/xyz/gozdzik/cryptolist/data/local/database/DatabaseModule.kt
+++ app/src/main/java/xyz/gozdzik/cryptolist/data/local/database/DatabaseModule.kt
@@ -1,23 +1,12 @@
 package xyz.gozdzik.cryptolist.data.local.database
 
 import android.content.Context
-import android.content.res.AssetManager
 import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
-import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import xyz.gozdzik.cryptolist.core.utils.JsonAssetReader
-import xyz.gozdzik.cryptolist.data.local.database.prepopulate.CurrencyInfoPrepopulateRepository
-import xyz.gozdzik.cryptolist.data.model.parsers.CurrencyInfoJsonParser
-import javax.inject.Provider
 import javax.inject.Singleton
 
 @Module
@@ -28,46 +17,15 @@ class DatabaseModule {
     @Singleton
     fun provideAppDatabase(
         @ApplicationContext appContext: Context,
-        currencyInfoDaoProvider: Provider<CurrencyInfoDao>,
-        currencyInfoPrepopulateRepository: CurrencyInfoPrepopulateRepository
     ) =
         Room.databaseBuilder(
             appContext,
             AppDatabase::class.java,
             APP_DATABASE_NAME
-        )
-            .addCallback(object : RoomDatabase.Callback() {
-                override fun onCreate(db: SupportSQLiteDatabase) {
-                    super.onCreate(db)
-                    CoroutineScope(Dispatchers.Default).launch {
-                        currencyInfoDaoProvider.get().insertAll(
-                            currencyInfoPrepopulateRepository.getAll()
-                        )
-                    }
-                }
-            })
-            .build().initializeDatabaseAfterBuild()
+        ).build()
 
     @Provides
     fun provideCurrencyInfoDao(appDatabase: AppDatabase) =
         appDatabase.currencyInfoDao()
-
-    @Provides
-    fun provideCurrencyInfoPrepopulateRepository(
-        jsonAssetReader: JsonAssetReader,
-        parser: CurrencyInfoJsonParser
-    ) = CurrencyInfoPrepopulateRepository(
-        jsonAssetReader, parser
-    )
-
-    @Provides
-    fun provideJsonAssetReader(assetManager: AssetManager) = JsonAssetReader(assetManager)
-
-    @Provides
-    fun provideAssetManager(@ApplicationContext appContext: Context): AssetManager =
-        appContext.assets
-
-    @Provides
-    fun provideCurrencyInfoJsonParser() = CurrencyInfoJsonParser(Gson())
 
 }
